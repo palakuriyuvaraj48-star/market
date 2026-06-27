bbd85ad580d88c615c278f908819350676699bc2
Yuvaraj <palakuriyuvaraj48@gmail.com>
2026-06-27 23:52:42 +0530

    Remove duplicate freshmart-pro project

--- freshmart-pro/database/seed/roles.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO roles (name, description) VALUES
-('ROLE_CUSTOMER', 'Customer storefront access'),
-('ROLE_ADMIN', 'Administrative operations access'),
-('ROLE_VENDOR', 'Vendor portal access'),
-('ROLE_DELIVERY_PARTNER', 'Delivery partner portal access');
